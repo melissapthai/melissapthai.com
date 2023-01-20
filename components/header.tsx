39078b5ce0715e875821544cde643a955586c922
Melissa Thai <melissapthai@gmail.com>
2023-01-20 14:40:41 -0800

    make header a little more responsive

--- components/header.tsx
+++ components/header.tsx
@@ -1,3 +1,4 @@
+import { useState, useCallback, useEffect } from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useTheme } from "next-themes";
@@ -13,10 +14,42 @@ export default function Header() {
   const ariaLabel = isDarkMode ? "Activate Light Mode" : "Activate Dark Mode";
   const alt = isDarkMode ? "sun icon" : "moon icon";
 
+  // Source: https://github.com/vercel/next.js/discussions/14810#discussioncomment-1709481
+  // Not proud of this but it'll do for now
+  const useMediaQuery = (width: number) => {
+    const [targetReached, setTargetReached] = useState(false);
+
+    const updateTarget = useCallback((e: any) => {
+      if (e.matches) {
+        setTargetReached(true);
+      } else {
+        setTargetReached(false);
+      }
+    }, []);
+
+    useEffect(() => {
+      const media = window.matchMedia(`(max-width: ${width}px)`);
+      media.addListener(updateTarget);
+
+      // Check on mount (callback is not called until a change occurs)
+      if (media.matches) {
+        setTargetReached(true);
+      }
+
+      return () => media.removeListener(updateTarget);
+    }, []);
+
+    return targetReached;
+  };
+
+  // Get small breakpoint as defined in tailwind docs: https://tailwindcss.com/docs/responsive-design
+  const isBreakpoint = useMediaQuery(640);
+  const name = isBreakpoint ? "MT" : "Melissa Thai";
+
   return (
     <header className="flex items-center lg:px-10">
       <div className="basis-1/3">
-        <h1>Melissa Thai</h1>
+        <h1>{name}</h1>
       </div>
       <nav className="basis-1/3" aria-label="Main Navigation">
         <div className="flex justify-center">
